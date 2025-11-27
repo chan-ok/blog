55bb56d92a95ccaf76a940840a1883fc2e545f6a
Kim Chanho <kiss.yagni.dry@gmail.com>
2025-11-28 01:32:55 +0900

    style: update data-disabled attribute selector for contact form button

--- src/features/contact-form.tsx
+++ src/features/contact-form.tsx
@@ -63,7 +63,7 @@ export default function ContactForm() {
             focusableWhenDisabled
             type="submit"
             disabled={!token || loading}
-            className="flex h-12 w-40 items-center justify-center rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base leading-6 font-medium text-gray-900 outline-0 select-none hover:bg-gray-100 focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:border-t-gray-300 active:bg-gray-200 active:shadow-[inset_0_1px_3px_rgba(0,0,0,0.1)] data-[disabled]:bg-gray-200 data-[disabled]:text-gray-500"
+            className="flex h-12 w-40 items-center justify-center rounded-md border border-gray-200 bg-gray-50 px-3.5 text-base leading-6 font-medium text-gray-900 outline-0 select-none hover:bg-gray-100 focus-visible:outline-2 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:border-t-gray-300 active:bg-gray-200 active:shadow-[inset_0_1px_3px_rgba(0,0,0,0.1)] data-disabled:bg-gray-200 data-disabled:text-gray-500"
           >
             {!token ? 'Check Robot' : loading ? 'Sending...' : 'Submit'}
           </Button>
