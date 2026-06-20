48a1f2ab601769701bda6cb31f5f2b9e37ef3868
Kim Chanho <kiss.yagni.dry@gmail.com>
2026-06-20 14:41:10 +0900

    Use typed i18next instance in locale provider

--- src/shared/locale/config.ts
+++ src/shared/locale/config.ts
@@ -1,10 +1,12 @@
-import * as i18next from 'i18next';
+import { createInstance, type i18n } from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import ja from './locales/ja.json';
 import ko from './locales/ko.json';
 
 export const resources = { ko, ja } as const;
+
+const i18next: i18n = createInstance();
 
 i18next.use(initReactI18next).init({
   resources: {
